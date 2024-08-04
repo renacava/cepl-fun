54695c75d10cdcd85095dba1107f315cd1165b37
renacava <renanocava@gmail.com>
2024-08-04 17:12:41 +0200

    Update main.lisp

--- main.lisp
+++ main.lisp
@@ -1,13 +1,24 @@
 (in-package #:cepl-fun)
 
 (defparameter *vert-gpu-array* nil)
+(defparameter *vert-gpu-index-array* nil)
 (defparameter *vert-array-buffer-stream* nil)
 
+(defun try-free (object)
+  (when object (free object)))
+
+(defun try-free-objects (&rest objects)
+  (mapcar #'try-free objects))
+
 (defun init ()
+  (try-free-objects *vert-gpu-array* *vert-gpu-index-array* *vert-array-buffer-stream*)
+  
+  (setf *vert-gpu-index-array* (make-gpu-array (list 0 1 2 0 2 3)))
   (setf *vert-gpu-array* (make-gpu-array
-                        (list (v! -0.5 0.5)
-                              (v! -0.5 -0.5)
-                              (v! 0.5 -0.5)
-                              (v! 0.5 0.5)))))
+                          (list (v! -0.5 0.5)
+                                (v! -0.5 -0.5)
+                                (v! 0.5 -0.5)
+                                (v! 0.5 0.5))))
+  (setf *vert-array-buffer-stream* (make-buffer-stream *vert-gpu-array* :index-array *vert-gpu-index-array*)))
 
 (defun main ())
